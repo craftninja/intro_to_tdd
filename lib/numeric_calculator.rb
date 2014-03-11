a0ecc5dd620fa7fc919bfeefa167160f4b0e0a8c
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-11 14:14:36 -0600

    Allows users to subtract two numbers

--- lib/numeric_calculator.rb
+++ lib/numeric_calculator.rb
@@ -2,4 +2,8 @@ class NumericCalc
   def add(num_a, num_b)
     num_a+num_b
   end
+
+  def subtract(num_a, num_b)
+    num_a-num_b
+  end
 end
--- spec/numeric_calculator_spec.rb
+++ spec/numeric_calculator_spec.rb
@@ -3,8 +3,13 @@ require "numeric_calculator"
 
 describe NumericCalc do
   it "allows users to add two numbers" do
-    @calculator = NumericCalc.new
-    expect(@calculator.add(4, 5)).to eq 9
+    calculator = NumericCalc.new
+    expect(calculator.add(4, 5)).to eq 9
+  end
+
+  it "allows users to subtract two numbers" do
+    calculator = NumericCalc.new
+    expect(calculator.subtract(9, 6)).to eq 3
   end
 
 
