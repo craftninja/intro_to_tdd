2b128453512b73c828dce536d9b55fc1aa53e88c
EmilyPlatzer <emilyplatzer@gmail.com>
2014-03-10 20:10:42 -0600

    Refactoring, key returns value inside class

--- spec/key_value_store_spec.rb
+++ spec/key_value_store_spec.rb
@@ -1,10 +1,20 @@
 require "rspec/core"
 require "key_value_store"
 
+
 describe KeyValueStore do
+
+  before do
+    @newstore = KeyValueStore.new
+    @newstore.new_pair("some key", "some value")
+    @newstore.new_pair("other key", "other value")
+  end
+
   it "will add a value to a key inside a class" do
-    newstore = KeyValueStore.new
-    newstore.new_pair("some key", "some value")
-    expect(newstore.check_store["some key"]).to eq "some value"
+    expect(@newstore.check_store["some key"]).to eq "some value"
+  end
+
+  it "will return a value for a specified key" do
+    expect(@newstore.check_store["some key"]).to eq "some value"
   end
 end
